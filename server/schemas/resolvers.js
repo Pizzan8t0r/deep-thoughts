4a4543264a11db7f80b0a9056c7010246ed7652f
Pizzan8t0r <https://github.com/Pizzan8t0r>
2023-05-29 18:07:26 -0700

    added the jwt functionality

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,4 @@
+const { AuthenticationError } = require('apollo-server-express');
 const { User, Thought } = require('../models');
 
 const resolvers = {
@@ -11,10 +12,26 @@ const resolvers = {
         return Thought.findOne({ _id });
       }
       Mutation: {
-        addUser: async () => {
-    
-        },
-        login: async () => {    
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+          
+            return user;
+          }
+          login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+          
+            if (!user) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+          
+            const correctPw = await user.isCorrectPassword(password);
+          
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+          
+            return user;
+          }
     // get all users
 users: async () => {
     return User.find()
